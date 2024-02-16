9cf168cacb30d854739e5c215544f8c1e39eb266
luixal <luixal@gmail.com>
2024-02-16 14:03:09 +0100

    Initial version 0.1.0

---
+++ main.mjs
@@ -0,0 +1,99 @@
+import Fastify from "fastify";
+import MqttClient from "./mqtt.mjs";
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const HTTP_API_KEY = process.env.HTTP_API_KEY;
+// mqtt env variables:
+const MQTT_HOST = process.env.MQTT_HOST;
+const MQTT_PORT = process.env.MQTT_PORT;
+const MQTT_PROTOCOL = process.env.MQTT_PROTOCOL;
+const MQTT_CLIENT_ID = process.env.MQTT_CLIENT_ID;
+const MQTT_USERNAME = process.env.MQTT_USERNAME;
+const MQTT_PASSWORD = process.env.MQTT_PASSWORD;
+const MQTT_TOPIC = process.env.MQTT_TOPIC;
+const MQTT_CONNECT_TIMEOUT = process.env.MQTT_CONNECT_TIMEOUT;
+const MQTT_RECONNECT_PERIOD = process.env.MQTT_RECONNECT_PERIOD;
+
+// fastify logger:
+const loggerFastify = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:mm:ss Z',
+        ignore: 'pid,hostname,reqId,responseTime,req,res',
+        messageFormat: '[HTTP][{reqId}] {msg} [{reqId}][{req.method}][{req.url}] [{req.remoteAddress}:{req.remotePort}] [{res.statusCode}][{responseTime}]'
+      }
+    },
+  },
+  production: true,
+  test: false
+};
+
+// create fastify api:
+const fastify = Fastify({
+  logger: loggerFastify[NODE_ENV] ?? true
+});
+
+// create mqtt client:
+const mqttClient = new MqttClient(
+  MQTT_HOST,
+  MQTT_PORT,
+  MQTT_PROTOCOL,
+  MQTT_CLIENT_ID,
+  {
+    clientId: MQTT_CLIENT_ID,
+    username: MQTT_USERNAME,
+    password: MQTT_PASSWORD,
+    reconnectPeriod: MQTT_RECONNECT_PERIOD,
+    connectTimeout: MQTT_CONNECT_TIMEOUT
+
+  }
+);
+
+// warning if no API-KEY is provided:
+if (!HTTP_API_KEY) fastify.log.warn('No API-KEY defined. You\'re going wild!');
+
+// API-KEY validation hook:
+fastify.addHook(
+  'onRequest',
+  (req, res, done) => {
+    if (req.params.apiKey === HTTP_API_KEY || req.headers['x-api-key'] === HTTP_API_KEY) {
+      done();
+    } else {
+      fastify.log.warn('Invalid API-KEY in request');
+      res.code(401).send({msg: 'Unauthorized - Invalid API-KEY'});
+    }
+  }
+);
+
+// GET route:
+fastify.get(
+  '/:apiKey',
+  async function handler(req, res) {
+    return await mqttClient.publish(MQTT_TOPIC, JSON.stringify(req.query), req.id);
+  }
+);
+
+// POST route:
+fastify.post(
+  '/',
+  async function handler(req, res) {
+    return await mqttClient.publish(MQTT_TOPIC, JSON.stringify(req.body), req.id);
+  }
+)
+
+
+// main code:
+console.log(`http2mqtt v.${process.env.npm_package_version}\n`);
+try {
+  // connect to mqtt broker:
+  await mqttClient.connect();
+  await fastify.listen({
+    port: 3000
+  });
+} catch(err) {
+  fastify.log.error(err);
+  await mqttClient.disconnect();
+  process.exit(1);
+}
---
+++ mqtt.mjs
@@ -0,0 +1,72 @@
+import mqtt from "mqtt";
+import pino from "pino";
+
+class MqttClient {
+
+  loggerConfig = {
+    development: {
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          translateTime: 'HH:mm:ss Z',
+          ignore: 'pid,hostname,reqId,responseTime,req,res,message',
+          messageFormat: '\u001b[34m[MQTT][reqId] {message}\u001b[0m'
+        }
+      },
+    },
+    production: true,
+    test: false
+  };
+
+  constructor(host, port, protocol, clientId, options) {
+    this.host = host;
+    this.port = port;
+    this.protocol = protocol;
+    this.clientId = clientId;
+    this.options = options;
+    // create logger:
+    this.logger = pino(this.loggerConfig[process.env.NODE_ENV || 'development']);
+    // build connect url:
+    this.url = `${protocol}://${host}:${port}`;
+  }
+
+  async connect() {
+    // this.client = await mqtt.connectAsync(this.url, this.options);
+    try {
+      this.logger.info({message: 'Connecting to MQTT broker: ' + this.url});
+      this.client = await mqtt.connectAsync(this.url, this.options);
+      if (this.isConnected()) this.logger.info({message: 'Connected to MQTT broker'});
+    } catch(err) {
+      this.logger.error({message: 'Error connecting to MQTT broker: ' + err.message});
+      throw new Error(err);
+    }
+  }
+
+  async publish(topic, message, reqId) {
+    try {
+      await this.client.publishAsync(topic, message);
+      this.logger.info({message: 'Message published'});
+      return {sucess: true, message: message};
+    } catch(err) {
+      this.logger.error({message: 'Error publishing message'});
+      return {success: false, error: err};
+    }
+  }
+
+  async disconnect() {
+    try {
+      await this.client.endAsync();
+      this.logger.info({message: 'Disconnected'});
+      return true;
+    } catch(err) {
+      this.logger.error({message: 'Could NOT disconnect'});
+      return false;
+    }
+  }
+
+  isConnected() {
+    return this.client?.connected;
+  }
+}
+
+export default MqttClient;
